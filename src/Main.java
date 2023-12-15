6546f3eaa6afc24e44756228a56c65725b7daea3
BT_1N3_31 <@poope>
2023-12-15 11:00:26 -0500

    qwdeqwdqwd

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
             System.out.println("i = " + i);
 
             System.out.println("Hi my name is Ambiya");
-            System.out.println("Hi, Selina bullies me everyday, but I still love her so it is a toxic relationship.");
+            System.out.println("Hi Shuchehchah Bullies me everyday");
             System.out.println("I am in 11th grade and in the Software major");
         }
     }
